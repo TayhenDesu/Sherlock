3bb8df5bd6e26682c39ac36913ddfd44f9ae525f
TayhenDesu <126518208+TayhenDesu@users.noreply.github.com>
2023-03-15 08:20:36 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -31,7 +31,7 @@ global.APIs = {
 
 //Api Key\\
 global.APIKeys = {
-	'https://zenzapis.xyz': 'your key',
+	'https://zenzapis.xyz': 'Sherlock Holmes 🕵🏻',
 }
 
 
@@ -54,23 +54,23 @@ global.doc5 = 'application/pdf'
 global.doc6 = 'application/vnd.android.package-archive'
 
 //owmner v card
-global.vcardowner = ['9660540637980'] //ur owner number
-global.ownername = "ABDUL RAHMAN🐼🍷 ٫" //ur owner name
-global.ytname = "YT: ABDUL RAHMAN" //ur yt chanel name
-global.socialm = "GitHub: ABDUL RAHMAN" //ur github or insta name
-global.location = "Egypt, Cairo, Aizawl" //ur location
+global.vcardowner = ['212709783771'] //ur owner number
+global.ownername = "Sherlock Holmes🕵🏻🍷 ٫" //ur owner name
+global.ytname = "Insta: itzz_sherlock" //ur yt chanel name
+global.socialm = "GitHub: TayhenDesu" //ur github or insta name
+global.location = "Morocco, Fes hakma l3alam" //ur location
 
 //bot bomdy 
-global.owner = ['9660540637980']
-global.ownertag = '9660540637980' //ur tag number
-global.botname = 'BOT ABDUL RAHMAN🐼🍷 ٫' //ur bot name
-global.linkz = "https://chat.whatsapp.com/LXZnV3rPFqD4ryPvG0Cyvvv" //your theme url which will be displayed on whatsapp
-global.websitex = "https://chat.whatsapp.com/LXZnV3rPFqD4ryPvG0Cyvvv" //ur website to be displayed
-global.botscript = 'https://chat.whatsapp.com/LXZnV3rPFqD4ryPvG0Cyvvv' //script link
-global.reactmoji = "🌚" //ur menu react emoji
-global.themeemoji = "🌞" //ur theme emoji
-global.packname = "⍣عبدالرحمن⍣" //ur sticker watermark packname
-global.author = "⍣9660540637980⍣" //ur sticker watermark author
+global.owner = ['212709783771']
+global.ownertag = '212709783771' //ur tag number
+global.botname = 'Sherlock🕵🏻🍷 ٫' //ur bot name
+global.linkz = "https://linktr.ee/Sher_lock" //your theme url which will be displayed on whatsapp
+global.websitex = "https://linktr.ee/Sher_lock" //ur website to be displayed
+global.botscript = 'https://linktr.ee/Sher_lock' //script link
+global.reactmoji = "🕵🏻" //ur menu react emoji
+global.themeemoji = "🕵🏼" //ur theme emoji
+global.packname = "⍣ Sherlock Holmes 🕵🏻 ⍣" //ur sticker watermark packname
+global.author = "⍣212709783771⍣" //ur sticker watermark author
 
 //Bot theme media
 global.thum = fs.readFileSync("./XeonMedia/theme/cheemspic.jpg") //ur thumb pic
@@ -79,7 +79,7 @@ global.err4r = fs.readFileSync("./XeonMedia/theme/cheemspic.jpg") //ur error pic
 global.thumb = fs.readFileSync("./XeonMedia/theme/cheemspic.jpg") //ur thumb pic
 
 //damtabase
-global.premium = ['9660540637980'] //ur premium numbers
+global.premium = ['212709783771'] //ur premium numbers
 global.ntilinkytvid = []
 global.ntilinkytch = []
 global.ntilinkig = []
@@ -108,7 +108,7 @@ global.mess = {
     success: 'تم يروحي ✓',
     admin: 'هذه الميزه للرجاله المشرفين,استرجل وجرب مره تانيه!! ',
     botAdmin: 'يجب أن يكون البوت مسؤولاً أولاً!',
-    owner: 'هذه الميزة للمالك فقط!',
+    owner: 'هذه الميزة لشيرلوك فقط!',
     group: 'لميزة تستخدم فقط للمجموعات!',
     private: 'الميزه المستخدمة فقط للدردشة الخاصة!',
     bot: 'هذه الميزة مخصصة للبوت فقط',
@@ -118,7 +118,7 @@ global.mess = {
     endLimit: 'Your Daily Limit Has Expired, The Limit Will Be Reset Every 12 Hours',
     ban: 'لقد تم حظرك من قبل المالك ، إذا كنت تريد أن تكون غير محظور ، مالك الدردشة.',
     nsfw: 'لم يتم تنشيط ميزة nsfw ، يرجى الاتصال بالمسؤول للتفعيل',
-    banChat: 'تم حظر الروبوت في هذه المجموعة ، يرجى الاتصال بالمالك لإلغاء الحظر'
+    banChat: 'تم حظر الروبوت في هذه المجموعة ، يرجى الاتصال بشيرلوك لإلغاء الحظر'
 }
     global.limitawal = {
     premium: "Infinity",
